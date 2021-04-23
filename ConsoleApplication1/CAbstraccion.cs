028da42ce10d72d23fe35ea7dd214da07a4b6592
FabioVlad <81887721+FabioVlad@users.noreply.github.com>
2021-04-23 19:14:00 -0400

    Add files via upload

---
+++ ConsoleApplication1/CAbstraccion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class CAbstraccion
+    {
+        IBridge implementacion;
+        Dictionary<string, double> notas;
+
+        public CAbstraccion(IBridge pImp, Dictionary<string, double> pNota)
+        {
+            implementacion = pImp;
+            notas = pNota;
+        }
+        public CAbstraccion(int pTipo, Dictionary<string, double> pNota)
+        {
+            if (pTipo == 1)
+                implementacion = new CImplementacion1();
+            if (pTipo == 2)
+                implementacion = new CImplementacion2();
+            
+
+            notas = pNota;
+        }
+
+        public void MostrarTotales()
+        {
+            implementacion.MostrarTotales(notas);
+        }
+
+        public void Listar()
+        {
+            implementacion.ListarNotas(notas);
+        }
+    }
+}
---
+++ ConsoleApplication1/IBridge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    interface IBridge
+    {
+        void MostrarTotales(Dictionary<string, double> pNotas);
+        void ListarNotas(Dictionary<string, double> pNotas);
+    }
+}
